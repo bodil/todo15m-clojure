6dcc2f453574b2316b30d47ae0a1f8e66d28252f
Bodil Stokke <bodil@bodil.tv>
2012-04-06 13:41:08 +0200

    Removed obsolete dependency.

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "The traditional todo webapp, in Clojure"
   :url "http://github.com/bodil/todo15m-clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/tools.logging "0.2.3"]
                  [noir "1.3.0-alpha10"]
                  [com.novemberain/monger "1.0.0-beta2"]]
   :main ^{:skip-aot true} todo15m.server)
